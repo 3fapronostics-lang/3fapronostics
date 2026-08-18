637d67c2aac3b8fb4378ab0680c7432743c09c21
3fapronostics-lang <3fapronostics@gmail.com>
2026-08-18 22:27:59 +0200

    Update ChampionPick.js

--- components/ChampionPick.js
+++ components/ChampionPick.js
@@ -116,7 +116,7 @@ export default function ChampionPick({ champ, teams, user, isAdmin, label }) {
                 (locked ? ' opacity-60' : '')
               }
             >
-              <TeamLogo team={t} size={16} /> {t.name}
+              <TeamLogo team={t} size={22} /> {t.name}
             </button>
           ))}
         </div>
